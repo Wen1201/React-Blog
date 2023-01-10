47e9f126c74911a90c46269695a8c640bde822cb
Wen <gongwen1201@gmail.com>
2023-01-10 15:22:45 +1100

    create write page

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,22 @@
+import './write.css'
+
+export default function Write() {
+  return (
+    <div className='write'>
+        <img className='writeImg' src="https://images.unsplash.com/photo-1493780474015-ba834fd0ce2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1442&q=80" alt="" />
+        <form className='writeForm'>
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    <i className="writeIcon fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id='fileInput' style={{display: "none"}}/>
+                <input type="text"  placeholder='Title' className='writeInput' autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                <textarea placeholder='Tell your story...' type="text" className='writeInput writeText'></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+    </div>
+  )
+}
